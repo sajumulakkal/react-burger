e5fc64b00d6354e16a4e29748f3c430200324d5d
Saju Mulakkal Joseph <sajumulakkal@hotmail.com>
2021-01-06 21:54:06 +0800

    build issue addressed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Aux from '../../hoc/MyAux/_Aux';
 import Burger from '../../components/Burger/Burger';
